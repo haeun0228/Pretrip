6a6dbf64fe74b5586eb285af0baa85b6888e5fc0
m2nseop <mini071663@gmail.com>
2024-07-29 20:08:36 +0900

    카카오맵띄우고마커생성까지완_지현님파일에덧붙임

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+//루트 경로는 일단 index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// /detail 경로 요청 시 templates/detail.html 서빙
+app.get('/detail', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'templates', 'detail.html'));
 });
 
 app.listen(PORT, () => {
