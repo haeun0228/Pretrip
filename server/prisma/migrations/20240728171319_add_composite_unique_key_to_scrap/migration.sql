224f4a6779852aeadeb776cf95e153f883952822
m2nseop <mini071663@gmail.com>
2024-07-29 22:44:29 +0900

    migration에러해결

--- server/prisma/migrations/20240728171319_add_composite_unique_key_to_scrap/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[scrapperId,scrapPlaceId]` on the table `Scrap` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Scrap_scrapperId_scrapPlaceId_key` ON `Scrap`(`scrapperId`, `scrapPlaceId`);
